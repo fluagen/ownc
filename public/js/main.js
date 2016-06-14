4f45989fa9a74c8110d9c6ca79c7c32f893f6dd6
majie <maj@calis.edu.cn>
2016-06-14 18:54:17 +0800

    add replyOne

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,35 @@
 (function($) {
+    var md = window.markdownit({
+        html: false, // Enable HTML tags in source
+        xhtmlOut: false, // Use '/' to close single tags (<br />)
+        breaks: false, // Convert '\n' in paragraphs into <br>
+        linkify: true, // Autoconvert URL-like text to links
+        typographer: true, // Enable smartypants and other sweet transforms
+    });
+    var linkify = md.linkify;
+    linkify.add('@', {
+        validate: function(text, pos, self) {
+            var tail = text.slice(pos);
 
+            if (!self.re.twitter) {
+                self.re.twitter = new RegExp(
+                    '^([a-zA-Z0-9_]){1,15}(?!_)(?=$|' + self.re.src_ZPCc + ')'
+                );
+            }
+            if (self.re.twitter.test(tail)) {
+                // Linkifier allows punctuation chars before prefix,
+                // but we additionally disable `@` ("@@mention" is invalid)
+                if (pos >= 2 && tail[pos - 2] === '@') {
+                    return false;
+                }
+                return tail.match(self.re.twitter)[0].length;
+            }
+            return 0;
+        },
+        normalize: function(match) {
+            match.url = '/user/' + match.url.replace(/^@/, '');
+        }
+    });
     var ownc = {
         init: function() {
             var self = this;
@@ -49,7 +79,6 @@
                 $('#preview-btn').toggle(false);
                 $('#edit-btn').toggle(true);
 
-                var md = window.markdownit();
                 $('.preview-box').html(md.render($('.editor-text').val()));
             });
         },
@@ -87,7 +116,7 @@
                 $('#fileupload').click();
             });
         },
-        alertMessageBar: function(obj, type, msg) {
+        alertMessageBar: function(box, type, msg) {
             if (!type) {
                 type = "alert-info";
             }
@@ -95,7 +124,7 @@
                 "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\"> &times;" +
                 "</button>" + msg +
                 "</div>";
-            obj.prepend(html);
+            box.prepend(html);
             return;
         },
         initReplySubmit: function() {
@@ -108,10 +137,18 @@
                 }
                 $('.reply-form').submit();
             });
+        },
+        initOptReply: function(){
+            $('.opt-reply').click(function(){});
         }
 
     };
+    var replyOne = function (username){
+        var mention = '\n' + '@' + username + ' ';
+        ownc.insertStringToEditor(mention);
+    };
     window.ownc = ownc;
+    window.replyOne = replyOne;
 })(jQuery);
 
 $(document).ready(function() {
